00a62b64e338df8dbb057a8cbe13790a56d363f3
Mohammd Shihab Hossain <shihab.hossain.2003@gmail.com>
2025-02-06 03:49:25 +0000

    BIsmillah

---
+++ my-app/app/blog/How-not-to-pickup-a-gril-form-dungan/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Script from 'next/script';
+const page = () => {
+  return (
+    <Script src="\source.js"> DO or Die</Script>
+  )
+}
+
+export default page
---
+++ my-app/public/source.js
@@ -0,0 +1,2 @@
+console.log("I am so lonely")
+alert("GIve up and die")
